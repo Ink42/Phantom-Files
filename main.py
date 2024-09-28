4b53983100f7ea2a336f7fd06ddedb95bb58cd25
Ink42 <lindomash001@gmail.com>
2024-09-28 15:36:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,38 @@
 import os
 import random
 import shutil
-
-off_zones = []
+import json
+main_dir = os.getcwd()
 
 def migrate(new_dir):
-    current_dir = os.getcwd()
-    shutil.move(current_dir, new_dir)
+    # print(new_dir)
+    update_history(new_dir)
+    shutil.move(main_dir, new_dir)
+
+def update_history(new_dir):
+    print("s")
+    history_file_path = os.path.join(main_dir, "history.json")
+    with open(history_file_path, "r") as file:
+        cool = json.load(file)
+ 
+    cool["path"] = new_dir
+    cool["history"]["prev path"] = main_dir  
+    print(cool)
+    with open(history_file_path, "w") as file:
+        json.dump(cool, file, indent=4)
 
 def find_possible_paths(starting_point):
     legal_paths = []
     os.chdir(starting_point)
     
-    while True:
+    for i in range(5):
         new_path = scout_dir()
         if new_path:  
             legal_paths.append(new_path)
-            print(f"Found directory: {new_path}")
         else:
-            break  
+            break
+    if legal_paths:
+        migrate(random.choice(legal_paths))  
 
 def scout_dir() -> str:
     directories = [x for x in os.listdir() if os.path.isdir(x) and "." not in x]
